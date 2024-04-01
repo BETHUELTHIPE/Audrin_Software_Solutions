f84eee79e4cc19bfe315088c886601c2749af7e9
Bethuel <moukangwebethuel@gmail>
2024-04-01 02:46:00 +0200

    updated settings file

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -12,29 +12,24 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
-from django.core.management.utils import get_random_secret_key
-from environ import Env
-import dj_database_url
-#import django_heroku
-# Instantiate Env class
-env = Env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--4iws!uc#2j2cwb2llj!)k&lw3=+zy^2cxqrdj2h=nai8eiq*8'
-#SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-#DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['audrinsoftwaresolutions']
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
 
 # Application definition
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -49,16 +44,15 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'storages'
+
 ]
 
-# Django Rest Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
 
-# Simple JWT settings
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
@@ -88,11 +82,15 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-# Middleware settings
+
+# TESTING
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -101,10 +99,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# Root URL configuration
 ROOT_URLCONF = 'devsearch.urls'
 
-# Template configuration
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -123,33 +119,22 @@ TEMPLATES = [
     },
 ]
 
-# WSGI application
-#WSGI_APPLICATION = 'devsearch.wsgi.application'
-WSGI_APPLICATION ='gunicorn devsearch.wsgi:application'
-# Database configuration
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': 'AfricaDataEngineersDB',
-        #'USER': 'postgres',
-        #'PASSWORD': '23498812',
-        #'HOST': 'localhost',
-        #'PORT': '5432',
-    #}
-#}
+WSGI_APPLICATION = 'devsearch.wsgi.application'
 
-#DATABASES = {
-    #'default': dj_database_url.config(
-         #Replace this value with your local database's connection string.
-        #default='postgres://africadataengineersdb_user:FU3pL9OrOjKtoKfMuaxJTamfqaKSrA3C@dpg-co0a0mq1hbls73brbl60-a.oregon-postgres.render.com/africadataengineersdb',
-        #conn_max_age=600
-    #)
-#}
 
-#database_url=os.environ.get("DATABASE_URL")
-#DATABASES["default"]=dj_database_url.parse("database_url")
-#postgres://africadataengineersdb_user:FU3pL9OrOjKtoKfMuaxJTamfqaKSrA3C@dpg-co0a0mq1hbls73brbl60-a.oregon-postgres.render.com/africadataengineersdb
-# Password validation settings
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'devsearch',
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASS'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': '5432',
+#     }
+# }
+
 
 DATABASES = { 
     'default': { 
@@ -162,6 +147,15 @@ DATABASES = {
     } 
 } 
 
+
+
+
+
+
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -178,26 +172,33 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization settings
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
-# CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
 
-# E-mail configuration
-EMAIL_BACKEND = env("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
-EMAIL_PORT = env("EMAIL_PORT", default=587)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env("USER_EMAIL", default="bethuelthipe@gmail.com")
-EMAIL_HOST_PASSWORD = env("USER_PASSWORD")
+EMAIL_HOST_USER = 'YOUR-EMAIL'
+EMAIL_HOST_PASSWORD = 'YOUR-EMAIL-PASSWORD'
 
-DEFAULT_FROM_EMAIL = 'Audrin Software Solutions <django.bethuelthipe@gmail.com>'
 
-# Static files configuration
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
@@ -208,19 +209,21 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-# This production code might break development mode, so we check whether we're in DEBUG mode
-#if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
 # Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Conditional debug setting for production
-if os.getcwd() == '/apps':
+
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# AWS_QUERYSTRING_AUTH = False
+# AWS_S3_FILE_OVERWRITE = False
+
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = 'devsearch-bucket'
+
+
+if os.getcwd() == '/app':
     DEBUG = False

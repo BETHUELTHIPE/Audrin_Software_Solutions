55d809d42eccbab0b6341838e6b7057a224e55dd
Bethuel <moukangwebethuel@gmail>
2024-04-01 12:52:44 +0200

    added email configarations

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -183,7 +183,7 @@ CORS_ALLOW_ALL_ORIGINS = True
  #Get user email and password from environment variables or use default values
 # Email configuration settings:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'moukangwebethuel@mail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'moukangwebethuel@mail.com'  # Enter your GMAIL address
